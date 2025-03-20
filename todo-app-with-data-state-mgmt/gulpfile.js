1ee1161da6b2987e6bb11a011fdae4903d6e8823
Murali Varath <muralitharan.balanzer@gmail.com>
2025-03-19 20:23:36 -0400

    project setup with header and footer

--- todo-app-with-data-state-mgmt/gulpfile.js
+++ todo-app-with-data-state-mgmt/gulpfile.js
@@ -52,6 +52,7 @@ gulp.task("browser-sync", function () {
     server: {
       baseDir: "dist",
     },
+    reloadDelay: 1000,
   });
 
   gulp.watch("./src/**/*.html", gulp.series(["build"])).on("change", reload);
